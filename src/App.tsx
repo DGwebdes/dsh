d0ead54695360ee4110399aa9e631fe13d83d9c9
DGwebdes <alan.bilik8@gmail.com>
2026-02-02 10:58:35 +0000

    most of the process is automated. local -> github-dev-repo -> ghactions to build -> prod-repo -> cpanel -> Manual step -> cpanel.yml runs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Button from "./Button";
+import Component from "./Component";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -7,14 +8,15 @@ function App() {
         setCount(count + 1);
     }
     return (
-        <>
+        <div>
             <h1>cPanel + Github workflow</h1>
             <p>A small update to test</p>
             <Button
                 handleClick={handleClick}
                 count={count}
             />
-        </>
+            <Component />
+        </div>
     );
 }
 
---
+++ src/Component.tsx
@@ -0,0 +1,3 @@
+export default function Component() {
+    return <div>By this point it should be everything mostly automated. </div>;
+}
